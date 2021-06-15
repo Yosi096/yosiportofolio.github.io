31e11627b46e0878e38f0c28316d0ed222708202
yosimardianti <yorermrs@gmail.com>
2021-06-15 14:04:32 +0700

    make it better

---
+++ index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Portfolio Website</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">MyPorto<span>folio.</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#teams" class="menu-btn">Teams</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+      <div class="max-width">
+          <div class="row">
+            <div class="home-content">
+                <div class="text-1">Hello, nama saya</div>
+                <div class="text-2">Yosi Mardianti</div>
+                <div class="text-3">dan saya <span class="typing"></span></div>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/yosi.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">Saya Yosi Mardianti dan saya <span class="typing-2"></span></div>
+                    <p>Saya merupakan S1 Teknik Informatika lulusan dari Institut Teknologi Sumatera tahun 2021. Saya berkuliah sejak Oktober 2017 hingga Juli 2021. 
+                        Saat ini saya masih mencari perkerjaan dan mengasah <i>skill</i> saya dengan mengikuti beberapa pelatihan yang ada seperti BPPTIK, Digital Talent dll serta dengan belajar mandiri. Untuk fokus kemampuan yang saya asah yaitu <i>Back-end, UX Researcher</i> dan <i>Scrum Master</i>
+                    </p>
+                    <a href="files/mycv.pdf">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+    <div class="max-width">
+            <h2 class="title">My services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Backend</div>
+                        <p>Dengan menggunakan Node.js Framework Expres.js/Nest.js dan database MongoDB/MySql</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-blog"></i>
+                        <div class="text">Web</div>
+                        <p>Dengan menggunakan HTML/React JS, front-end sederhana.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fab fa-android"></i>
+                        <div class="text">Android</div>
+                        <p>Front-end android sederhana menggunakan Android Studio dengan Java.</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+        <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">Keterampilan & Pengalaman.</div>
+                    <p>Saat ini saya telah mempelajari beberapa bahasa pemrograman dan penyelesaian beberapa proyek dari tugas besar kuliah dan tugas akhir saya.
+                        Selain itu juga saya telah melaksanakan <i>Internship</i> sebagai <i>Backend</i> dan <i>Android Developer</i>. Saya juga mengikuti pelatihan yang diadakan BPPTIK dan Digitil Talent. Saya juga mengikuti kursus dari Coursera dan Dicoding. Serta 
+                        saya juga belajar mandiri dari beberapa tutorial dan hasil perkuliahan saya. Saya juga memiliki kemampuan basic pada bidang jaringan karena sebelumnya saya SMK Jaringan. 
+                    </p>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>ReactJs</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MongoDB</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>UI Figma</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">My certificate</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="images/sertif-1.jpg" alt="">
+                        <div class="text">Huawei Certification</div>
+                        <p>Routing and Switching.</p>
+                        <a href="images/sertif-1.jpg">Detail</a>
+                    </div>
+                </div>
+                <div class="card" >
+                    <div class="box">
+                        <img src="images/sertif-3.jpg" alt="">
+                        <div class="text">Pelatihan BPPTIK</div>
+                        <p>Junior Network Administrator.</p>
+                        <a href="images/sertif-3.jpg">Detail</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/sertif-2.jpg" alt="">
+                        <div class="text">Pelatihan BPPTIK</div>
+                        <p>Junior Web Developer.</p>
+                        <a href="images/sertif-2.jpg">Detail</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/sertif-4.jpg" alt="">
+                        <div class="text">Pelatihan BPPTIK</div>
+                        <p>Junior Office Operator</p>
+                        <a href="images/sertif-4.jpg">Detail</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/sertif-5.jpg" alt="">
+                        <div class="text">Coursera</div>
+                        <p>Build an E-commerce Dashbord With Figma.</p>
+                        <a href="images/sertif-5.jpg">Detail</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/sertif-7.jpg" alt="">
+                        <div class="text">Progate</div>
+                        <p>Kursus HTML dan CSS.</p>
+                        <a href="images/sertif-7.jpg">Detail</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/sertif-6.jpg" alt="">
+                        <div class="text">Sertifikat Kompeten</div>
+                        <p>Kompetensi Keahlian Kejuruan.</p>
+                        <a href="images/sertif-6.jpg">Detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Yosi Mardianti</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">Bandar Lampung, Lampung</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">yorermrd@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form class="contact-form" action="#" method="POST">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" class="fullname" placeholder="Name">
+                            </div>
+                            <div class="field email">
+                                <input type="text" class="email-input" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" class="subject" placeholder="Subject">
+                        </div>
+                        <div class="field textarea">
+                            <textarea class="message" cols="30" rows="10" placeholder="Message.."></textarea>
+                        </div>
+                        <div class="button-area">
+                            <div class="error-box"></div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By yosi</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
